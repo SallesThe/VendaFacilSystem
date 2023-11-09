3e4f4743297a013cc371f4122571d949911ff566
gabriels-devs <gabrielferreira.devs@gmail.com>
2023-11-08 21:58:28 -0300

    feat: implementing login screen and styling

---
+++ app/views/pages/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Venda Fácil</title>
+    <link rel="stylesheet" href="../../../public/css/login-style.css">
+</head>
+<body>
+    <div class="page">
+        <form method="post" class="form-login">
+            <h1>LOGIN</h1>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="Digite aqui seu email" autofocus="true">
+            <label for="password">Senha</label>
+            <input type="password" placeholder="Digite aqui sua senha">
+            <input type="submit" value="Acessar" class="button">
+            <input type="submit" value="Cadastrar" class="button">
+            <a href="#">Esqueci minha senha</a>
+        </form>
+    </div>
+</body>
+</html>
